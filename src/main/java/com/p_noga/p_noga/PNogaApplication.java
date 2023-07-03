a8e29166a13cfbec0ad63af3345a28bd6d6d9ab0
msk102948 <msk102948@gmail.com>
2023-07-03 21:11:28 +0900

    first commit

---
+++ src/main/java/com/p_noga/p_noga/PNogaApplication.java
@@ -0,0 +1,13 @@
+package com.p_noga.p_noga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PNogaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PNogaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/p_noga/p_noga/PNogaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p_noga.p_noga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PNogaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
